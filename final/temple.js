b2a0e0a958e27bff17eebe4769182b2f30b95c2f
laurenshelley <lauren.r.shelley@gmail.com>
2020-07-18 17:26:56 -0600

    UPODATE

--- final/temple.js
+++ final/temple.js
@@ -13,14 +13,21 @@ fetch(requestURL)
         for (let i = 0; i < locations.length; i++) {
             let card = document.createElement('section');
             let h4 = document.createElement('h4');
+            let p = document.createElement('p');
             let image = document.createElement('img');
+            // let lineBreak = document.createElement ("br");
             image.setAttribute('src',locations[i].imageurl);
             
 
             h4.textContent = locations[i].name
+            p.textContent = locations[i].address 
+         
+            p.textContent = locations[i].phone 
 
             card.appendChild(h4);
             card.appendChild(image);
+            card.appendChild(p);
+            card.appendChild(p);
 
             document.querySelector('div.cards').appendChild(card);
         }
